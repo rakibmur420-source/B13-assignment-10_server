60076933bf93cb56baed6640c264b725fa4ed18c
rakib <rakibmur420@gmail.com>
2026-06-23 19:59:28 +0600

    Fix CORS for Netlify deployment

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+  origin: [
+    process.env.CLIENT_URL,
+    'http://localhost:3000',
+    'https://lustrous-klepon-7a3daf.netlify.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
